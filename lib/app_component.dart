c6e7917300b80d4ddac55d07f771b0fc8c55e290
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2019-11-05 23:22:18 +0300

    multiple components

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,13 +1,13 @@
 import 'package:angular/angular.dart';
-import 'package:angular_forms/angular_forms.dart';
-import 'src/hero.dart';
-import 'src/mock_heroes.dart';
+import 'src/hero/hero.dart';
+import 'src/hero/heroes_fixture.dart';
+import 'src/hero/hero_component.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
   styleUrls: ['app_component.css'],
-  directives: [coreDirectives, formDirectives],
+  directives: [coreDirectives, HeroComponent],
 )
 class AppComponent {
   var title = 'Hello';
